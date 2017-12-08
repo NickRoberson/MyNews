6a639b8a3a1f2057d9c0c533c98de20614700ec5
nicholasroberson <roberson@grinnell.edu>
2017-12-08 14:06:08 -0600

    got working api's for news and articles

---
+++ index.js
@@ -0,0 +1,49 @@
+init();
+
+function init() {
+    searchArticles("post decolonization");
+    searchNews("bitcoin");
+}
+
+function searchNews(query_string) {
+    
+    var call_url = "https://newsapi.org/v2/everything?q=" + query_string + "&apiKey=c0fa8e12254a46a1955e24800e9b9ff6";
+    $.ajax({
+            url : call_url,
+            type : 'GET',
+            data : {
+            },
+            success : function(data) {              
+                console.log(data);
+            },
+            error : function(request,error)
+            {
+                console.log("Request: "+JSON.stringify(request));
+            }
+        });}
+
+function searchArticles(query_string) {
+
+    var call_url = "https://doaj.org/api/v1/search/articles/" + query_string + "?pageSize=100";
+    $.ajax({
+            url : call_url,
+            type : 'GET',
+            data : {
+                'numberOfWords' : 10
+            },
+            success : function(data) {              
+                console.log(data);
+                var url = data.results[0].bibjson.link[0].url;
+                console.log(url);
+                if(url.includes(".pdf")) {
+                    $('#media_body').html('<embed src="' + url + '" width="100%" height="100%" type="application/pdf">');
+                } else {
+                    $('#media_body').html('<iframe src="' + url + '" width="100%" height="100%">');
+                }
+            },
+            error : function(request,error)
+            {
+                console.log("Request: "+JSON.stringify(request));
+            }
+        });
+}
